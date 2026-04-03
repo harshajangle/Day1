c24451a4810c439ccc5d71d58cb45f9ded6633b0
harshajangle <ce24dse021@ybit.ac.in>
2026-04-03 10:13:45 +0530

    Add files via upload

---
+++ ds.py
@@ -0,0 +1,243 @@
+# math=50
+# name="Harsha"
+# pi=3.14
+# result=True
+# print(type(math))
+# print(type(name))
+# print(type(pi))
+# print(type(result))
+
+# math=50
+# name="Harsha"
+# pi=3.14
+# result=True
+# print(id(math))
+# print(id(name))
+# print(id(pi))
+# print(id(result))
+
+
+# math=50
+# chem=50
+# print(id(math))
+# print(id(chem))
+
+# math=50
+# chem=50
+# phy=50
+# hindi=40
+# print(id(math))
+# print(id(chem))
+# print(id(phy))
+# print(id(hindi))
+
+# list=[[1,2,3],
+#       [4,5,6],
+#       [7,8,9]]
+
+# print(2+2)
+# print("2"+"2")
+# val1=int(input("Enter value of val1:"))
+# val2=int(input("Enter value of val2:"))
+# print(val1 +val2)
+
+# print(int(3.14))
+# print(int(True))
+# print(int(False))
+# print(int("4"))
+
+# print(float(3))
+# print(float(True))
+# print(float(False))
+# print(float(4.22))
+# print(float("4"))
+
+# print(complex(3))
+# print(complex(12.5))
+# print(complex(True))
+# print(complex(False))
+# print(complex("5"))
+# print(complex("5.6"))
+# print(complex(5,-3))
+# print(complex(True,False))
+
+# print(bool(0))
+# print(bool(15))
+# print(bool(3.14))
+# print(bool(0.0))
+# print(bool(1+2j))
+# print(bool(0+0j))
+# print(bool(-1))
+# print(bool(False))
+# print(bool(True))
+# print(bool())
+
+#val1=int(input("Enter value of val1:"))
+# val2=int(input("Enter value of val2:"))
+# print("Before swapping val1=",val1, "val2=",val2)
+# temp=val1
+# val1=val2
+# val2=temp
+# print("After swapping val1=",val1, "val2=",val2)
+
+# val1=int(input("Enter value of val1:"))
+# val2=int(input("Enter value of val2:"))
+# print("Before swapping val1=",val1, "val2=",val2)
+# val1=val1+val2
+# val2=val1-val2
+# val1=val1-val2
+# print("After swapping val1=",val1, "val2=",val2)
+
+# a=int(input("Enter first num1:"))
+# b=int(input("Enter first num2:"))
+# print("Before swapping num1=",a, "num2=",b)
+# a=a+b
+# b=a-b
+# a=a-b
+# print("After swapping num1=",a, "num2=",b)
+
+# p1=int(input("Enter the marks of p1:"))
+# p2=int(input("Enter the marks of p2:"))
+# p3=int(input("Enter the marks of p3:"))
+# total=p1+p2+p3
+# per =total/3.0
+# print("total=",total)
+# print("percentage=",per)
+
+# p_amount=int(input("enter principal amount:"))
+# roi=int(input("enter rate of interest:"))
+# time=int(input("enter the duration of loan amount:"))
+# si=p_amount*roi*time/100
+# print("simple interest=",si)
+
+# h=float(input("Enter height:"))
+# inch=h*12
+# cm=inch*2.54
+# print("inch:",inch)
+# print("cm:",cm)
+
+# print(5/2)
+# print(5//2)
+
+# num=123
+# a=num%10
+# num=num//10
+# b=num%10
+# c=num//10
+# rev=a*100+b*10+c*1
+# print(rev)
+
+# num=123456
+# a=num%10
+# num=num//10
+# b=num%10
+# c=num//10
+# d=num//10
+# e=num//10
+# f=num//10
+# rev=a*100000+b*10000+c*1000+d*100+e*10+f*1
+# print(rev)
+
+# a=[10,20,30,40,50,60]
+# print(10 in a)
+# print(12 not in a)
+
+# a=10
+# b=20
+# print(a is b)
+# print(a is not b)
+
+
+# no=int(input("Enter any one no:"))
+# if no>0:
+#     print("positive number")
+# if no<0:
+#      print("negative number") 
+# if no==0:
+#      print("number is zero")
+
+# n1=int(input("enter num1:"))
+# n2=int(input("enter num2:"))
+# n3=int(input("enter num3:"))
+# n4=int(input("enter num4:"))
+# n5=int(input("enter num5:"))
+# if n1>n2 and n1>n3 and n1>n4 and n1>n5:
+#     print("Num1 is greater",n1)
+# if n2>n3 and n2>n4 and n2>n5: 
+#     print("Num 2 is greater",n2)
+# if n3>n4 and n3>n5:
+#     print("Number 3 is greater",n3)
+# if n4>n5:
+#     print("number 4 is greater",n4)
+# if n5>n1 and n5>n2 and n5>n3 and n5>n4:
+#     print("Number 5 is greater",n5)
+
+# username=input("enter username =")
+# password=input("enter password =")
+# if username == password:
+#     print('login successfully')
+# else:
+#     print("invalid credential")
+
+# phy=int(input("Enter physics marks="))
+# chem=int(input("Enter chemistry marks="))
+# math=int(input("Enter maths marks="))
+# total=phy+chem+math
+# per=total/3.0
+# print("total",total)
+# print("percentage",per)
+# gender=input("enter gender like M/F")
+
+# if per>=60 and gender=="M":
+#     print("He is eligible for placement")
+# else:
+#     print("He is eligible for data entry job")
+    
+
+# A = int(input("Enter no1: "))
+# B = int(input("Enter no2: "))
+# C = int(input("Enter no3: "))
+
+# if A > B:
+#     if A > C:
+#         print("Max value is:", A)
+#     else:
+#         print("Max value is:", C)
+# else:
+#     if B > C:
+#         print("Max value is:", B)
+#     else:
+#         print("Max value is:", C)
+
+
+# day=input("Enter your day name:")
+# if day=="saturday" or day=="SATURDAY" or day=="sunday" or day=="SUNDAY" :
+#     print("weekend")
+# else:
+#     print("working day")
+
+# ch=ord(input("enter any character"))
+
+# if ch>=65 and ch<=91:
+#     print("your entered character is in upper case")
+# elif ch>=97 and ch<=122:
+#     print("your entered character is in lower case")
+# elif ch>=48 and ch<=57:
+#     print("your entered character is digit")
+# else:
+#     print("your entered character is in special symbol")
+    
+    
+    
+# Amount=int(input("please enter amount for withdraw:"))
+# print("100 notes= ",Amount//100)
+# print("50 notes= ",(Amount%100) //50)
+# print("20 notes= ",((Amount%100)%50) //20)
+# print("10 notes= ",(((Amount%100)%50)%20) //10)
+# print("5 notes= ",((((Amount%100)%50)%20)%10) //5)
+
+
+
+
+
+      
